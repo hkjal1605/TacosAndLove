34aa5c68e5c29cc94ddac2f9a76dc45a469ad678
Harsh Kumar Jha <hkjal1605@gmail.com>
2020-08-22 23:44:16 +0530

    Created the functionality for the customer side

--- client/src/redux/booking/booking.sagas.js
+++ client/src/redux/booking/booking.sagas.js
@@ -6,9 +6,9 @@ import axios from "axios";
 import bookingActionTypes from "./booking.types";
 
 /* global Stripe */
-const stripe = Stripe(
-  "pk_test_51HH8C9DmhdHRcXnzhvTtC5tr3bHnZju3Y6NfARBqIiQ6WMuZm5pFEA12tXd5h4ZudV3eP1rN3Lp0PZlNOmtS5KHD00tw3OonWr"
-);
+// const stripe = Stripe(
+//   "pk_test_51HH8C9DmhdHRcXnzhvTtC5tr3bHnZju3Y6NfARBqIiQ6WMuZm5pFEA12tXd5h4ZudV3eP1rN3Lp0PZlNOmtS5KHD00tw3OonWr"
+// );
 
 export function* getCheckoutSessionAsync({ payload }) {
   try {
@@ -18,9 +18,9 @@ export function* getCheckoutSessionAsync({ payload }) {
 
     yield put(getCheckoutSession(session));
 
-    yield stripe.redirectToCheckout({
-      sessionId: session.data.session.id,
-    });
+    // yield stripe.redirectToCheckout({
+    //   sessionId: session.data.session.id,
+    // });
   } catch (err) {
     console.log(err);
   }
