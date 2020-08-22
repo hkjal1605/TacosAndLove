a8f257002a1cccf68449d06b6b9addd3da21666b
Harsh Kumar Jha <hkjal1605@gmail.com>
2020-08-22 23:03:37 +0530

    Created the functionality for the customer side

--- client/src/redux/booking/booking.actions.js
+++ client/src/redux/booking/booking.actions.js
@@ -1,12 +1,8 @@
 import bookingActionTypes from "./booking.types";
 
-const stripe = Stripe(
-  "pk_test_51HH8C9DmhdHRcXnzhvTtC5tr3bHnZju3Y6NfARBqIiQ6WMuZm5pFEA12tXd5h4ZudV3eP1rN3Lp0PZlNOmtS5KHD00tw3OonWr"
-);
-
 export const getCheckoutSessionStart = (cartId) => ({
   type: bookingActionTypes.GET_CHECKOUT_SESSION_START,
-  payload: { cartId, stripe },
+  payload: { cartId },
 });
 
 export const getCheckoutSession = (session) => ({
--- client/src/redux/booking/booking.sagas.js
+++ client/src/redux/booking/booking.sagas.js
@@ -5,6 +5,11 @@ import { getCheckoutSession } from "./booking.actions";
 import axios from "axios";
 import bookingActionTypes from "./booking.types";
 
+/* global Stripe */
+const stripe = Stripe(
+  "pk_test_51HH8C9DmhdHRcXnzhvTtC5tr3bHnZju3Y6NfARBqIiQ6WMuZm5pFEA12tXd5h4ZudV3eP1rN3Lp0PZlNOmtS5KHD00tw3OonWr"
+);
+
 export function* getCheckoutSessionAsync({ payload }) {
   try {
     const session = yield axios.get(
@@ -13,7 +18,7 @@ export function* getCheckoutSessionAsync({ payload }) {
 
     yield put(getCheckoutSession(session));
 
-    yield payload.stripe.redirectToCheckout({
+    yield stripe.redirectToCheckout({
       sessionId: session.data.session.id,
     });
   } catch (err) {
